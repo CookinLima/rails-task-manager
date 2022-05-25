f88de7d1f457fff41cb2aac0bfc1214d821031ef
Kenny Wong <kken.w@hotmail.com>
2022-05-25 16:10:04 +0800

    done but not all styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,28 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     # raise
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path()
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index"
+  # GET the restaurant creation form = 1 request
+  get "tasks/new", to: "tasks#new", as: :new_task
+  # POST the parameters to create a new restaurant = 1 request  
+  post "tasks", to: "tasks#create"
+
+  delete "tasks/:id", to: "tasks#destroy"
+
   get "tasks/:id", to: "tasks#show", as: :task
+
 end
 
